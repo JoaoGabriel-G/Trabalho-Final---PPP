b45aa4fa9850ae31dd261742e658b5f2ee95aa9c
JoaoGabriel-G <joaogabrielgdeo@gmail.com>
2026-07-19 23:28:25 -0300

    Delete QuestaoStrategy.java

--- QuestaoStrategy.java
+++
@@ -1,9 +0,0 @@
-package quiz.framework;
-
-import java.util.Scanner;
-
-public interface QuestaoStrategy {
-
-    public TipoQuestao getTipo();
-    public Questao criar(String enunciado, int quantidadeOpcoes, TipoQuestao tipo, Scanner sc);
-}
